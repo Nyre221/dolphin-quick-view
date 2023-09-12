5ca46489aacf6930c86cfcc9446f302ab0faa579
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2023-09-13 00:12:43 +0200

    added checks to commands

--- package creation/quick_view_package/INSTALL.sh
+++ package creation/quick_view_package/INSTALL.sh
@@ -5,19 +5,24 @@ HERE=${SELF%/*}
 
 
 mkdir -p ~/.config/quick_view
-
+[ "$?" != "0" ] && exit 1
 mkdir -p ~/.local/share/kservices5/ServiceMenus/
-
+[ "$?" != "0" ] && exit 1
 cp "$HERE"/quick_view.pyz ~/.config/quick_view
+[ "$?" != "0" ] && exit 1
 chmod +x ~/.config/quick_view/quick_view.pyz
-
+[ "$?" != "0" ] && exit 1
 cp "$HERE"/dolphin_quick_view_shortcut.sh ~/.config/quick_view
+[ "$?" != "0" ] && exit 1
 chmod +x ~/.config/quick_view/dolphin_quick_view_shortcut.sh
-
+[ "$?" != "0" ] && exit 1
 cp "$HERE"/quick_view.desktop ~/.local/share/kservices5/ServiceMenus/
+[ "$?" != "0" ] && exit 1
+
 
 #send desktop notification
-gdbus call --session   --dest=org.freedesktop.Notifications   --object-path=/org/freedesktop/Notifications   --method=org.freedesktop.Notifications.Notify   ""   0   "quickview"   "Quick View installed"   ""   '[]'   '{"urgency": <1>}'   5000
+if command -v gdbus >/dev/null 2>&1; then
+gdbus call --session   --dest=org.freedesktop.Notifications   --object-path=/org/freedesktop/Notifications   --method=org.freedesktop.Notifications.Notify   ""   0   "quickview"   "Quick View installed"   ""   '[]'   '{"urgency": <1>}'   5000 >/dev/null
+fi
 
-
-echo -e "\n\n\nINSTALLED"
+echo -e "\n\nINSTALLED"
