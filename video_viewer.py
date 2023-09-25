29dbdd17b72ff5ad530e469c50ae4daf1af7028d
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2023-09-26 00:57:44 +0200

    added shortcuts

--- video_viewer.py
+++ video_viewer.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import (QHBoxLayout,
-        QPushButton, QSlider, QStyle, QVBoxLayout, QWidget)
+        QPushButton, QSlider, QStyle, QVBoxLayout, QWidget, QShortcut)
 
 from PyQt5.QtCore import Qt,QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
@@ -10,9 +10,9 @@ class VideoViewer(QWidget):
     def __init__(self, parent=None):
         super(VideoViewer, self).__init__(parent)
         self.setFocus()
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-        videoWidget = QVideoWidget()
-
+        self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.videoWidget = QVideoWidget()
+        self.current_file = None
         #play button
         self.play_button = QPushButton()
         self.play_button.setFixedHeight(24)
@@ -23,7 +23,7 @@ class VideoViewer(QWidget):
         self.position_slider = QSlider(Qt.Horizontal)
         self.position_slider.setRange(0, 0)
         #in order to avoid glitch the media is paused when the slider is moved 
-        self.position_slider.sliderPressed.connect(lambda: self.mediaPlayer.pause())
+        self.position_slider.sliderPressed.connect(lambda: self.media_player.pause())
         self.position_slider.sliderReleased.connect(self.setPosition)
 
 
@@ -34,30 +34,37 @@ class VideoViewer(QWidget):
 
 
         main_layout = QVBoxLayout()
-        main_layout.addWidget(videoWidget)
+        main_layout.addWidget(self.videoWidget)
         main_layout.addLayout(controls_layout)
         self.setLayout(main_layout)
+        
+        self.media_player.setVideoOutput(self.videoWidget)
+        self.media_player.stateChanged.connect(self.mediaStateChanged)
+        self.media_player.positionChanged.connect(self.positionChanged)
+        self.media_player.durationChanged.connect(self.durationChanged)
 
-        self.mediaPlayer.setVideoOutput(videoWidget)
-        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
-        self.mediaPlayer.positionChanged.connect(self.positionChanged)
-        self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.set_shortcut()
 
 
     def open(self,path):
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(path)))
+        self.current_file = path
+        self.videoWidget.setFocus()
+        self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(path)))
         self.play()
 
 
     def play(self):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState :
-            self.mediaPlayer.pause()
-        else:
-            self.mediaPlayer.play()
+        if self.media_player.state() == QMediaPlayer.PlayingState:
+            self.media_player.pause()
+        elif self.media_player.state() == QMediaPlayer.PausedState:
+            self.media_player.play()
+        elif self.media_player.state() == QMediaPlayer.StoppedState:
+            self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(self.current_file)))
+            self.media_player.play()
 
 
     def mediaStateChanged(self, state):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+        if self.media_player.state() == QMediaPlayer.PlayingState:
             self.play_button.setIcon(
                     self.style().standardIcon(QStyle.SP_MediaPause))
         else:
@@ -74,10 +81,40 @@ class VideoViewer(QWidget):
     
 
     def setPosition(self):
-        self.mediaPlayer.setPosition(self.position_slider.value())
+        self.media_player.setPosition(self.position_slider.value())
         self.play()
 
 
+
+    def time_back(self):
+        #milliseconds: 10 sec
+        mseconds_back = 10000
+        current_pos = self.media_player.position()
+        if current_pos -  mseconds_back > 0:
+            self.media_player.setPosition(current_pos-mseconds_back)
+        else:
+            self.media_player.setPosition(0)
+
+
+
+    def time_forward(self):
+        total_time = self.media_player.duration()
+        #milliseconds: 10 sec
+        mseconds_forward= 10000
+        current_pos = self.media_player.position()
+        if mseconds_forward + current_pos < total_time:
+            self.media_player.setPosition( mseconds_forward + current_pos)
+        else:
+            # reaches end of video.
+            # -100 is used to trigger the "mediaStateChanged" signal.
+            self.media_player.setPosition(total_time-100)
+
+    def set_shortcut(self):
+        QShortcut(Qt.Key_Down, self).activated.connect(self.time_back)
+        QShortcut(Qt.Key_Up, self).activated.connect(self.time_forward)
+
+
+
     def hide(self) -> None:
-        self.mediaPlayer.stop()
+        self.media_player.stop()
         return super().hide()
