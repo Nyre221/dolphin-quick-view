c8055a150118bc31cab9f3fd7066231689a69f38
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2023-09-25 19:03:04 +0200

    Delete translations directory

--- translations/placeholder_translations/__init__.py
+++
@@ -1,8 +0,0 @@
-from translations.placeholder_translations import en_US
-from translations.placeholder_translations import it_IT
-
-
-available_placeholder_translations = {
-    "it_IT":it_IT.dictionary,
-    "en_US":en_US.dictionary,
-}
