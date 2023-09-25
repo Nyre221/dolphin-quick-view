c4ce1afea4b4459b9beefe5f37cc97902063b8e1
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2023-09-25 18:14:14 +0200

    added translations

---
+++ translations/placeholder_translations/en_US.py
@@ -0,0 +1,12 @@
+dictionary = dict(
+    loading_placeholder="Loading...",
+    size="Size",
+    last_modified="Last modified",
+    files="Number of files",
+    unknown="Unknown",
+    container_file_too_big="Preview not available: the file is too big.",
+    libreoffice_preview_unavailable="Preview not available.",
+    libreoffice_timeout="The document took too long to load.",
+    libreoffice_not_found="Libreoffice not found:",
+    libreoffice_file_not_found="File not found: Flatpak may not be configured correctly:",
+)
