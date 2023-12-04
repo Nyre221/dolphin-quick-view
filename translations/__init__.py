52fedc5bd5e497450d5c1813bf525820abed54f0
Danilo Agostini <nyre334@gmail.com>
2023-12-04 23:38:18 +0100

    added Dutch translation

--- translations/__init__.py
+++ translations/__init__.py
@@ -1,8 +1,10 @@
 from translations import en_US
 from translations import it_IT
+from translations import nl_NL
 
 
 available_placeholder_translations = {
     "it_IT":it_IT.dictionary,
     "en_US":en_US.dictionary,
+    "nl_NL":nl_NL.dictionary,
 }
