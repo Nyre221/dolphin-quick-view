27b5ddca6864497289eff92e06b415087855c9ba
Heimen Stoffels <vistausss@fastmail.com>
2023-12-04 20:08:12 +0100

    Update nl_NL.py

--- translations/nl_NL.py
+++ translations/nl_NL.py
@@ -9,6 +9,6 @@ dictionary = dict(
     libreoffice_preview_unavailable="Er is geen voorvertoning beschikbaar.",
     libreoffice_timeout="Het laden van het document duurde te lang.",
     libreoffice_not_found="Libreoffice is niet geïnstalleerd:",
-    libreoffice_file_not_found="Bestand niet aangetroffen - Wellicht is Flatpak niet goed ingesteld:",
+    libreoffice_file_not_found="Bestand niet aangetroffen - wellicht is Flatpak niet goed ingesteld:",
     unsupported_file_type="Dit bestandstype wordt niet ondersteund.",
 )
