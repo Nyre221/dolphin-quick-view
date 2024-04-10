51c98aaba02442bce0603319d4fcd56c5d5e3fad
Nyre221 <104171042+Nyre221@users.noreply.github.com>
2024-04-10 16:28:33 +0200

    Improved clipboard data restoration
    Now the file previously copied to the clipboard is restored.

--- package creation/quick_view_package/dolphin_quick_view_shortcut.sh
+++ package creation/quick_view_package/dolphin_quick_view_shortcut.sh
@@ -24,15 +24,61 @@ exit 1
 fi
 
 
+restore_clipboard_content() {
+local clipboard_content="${1}"
+# if the user had copied a file before opening the program, restoring just the text is not enough.
+# the "file:///" prefix indicates that the last item copied to the clipboard by the user was a file or folder.
+if   ! [  $(echo "${clipboard_content}" | grep "file:///") ] ; then
+# plain text, klipper is enough
+dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.setClipboardContents string:"${clipboard_content}"
+return
+fi
+
+binary_exec=""
+copy_command=""
+
+# check if it is using wayland or X11
+if [ $(echo $XDG_SESSION_TYPE | grep -i "wayland") ];then
+# wayland
+binary_exec="wl-copy"
+copy_command='wl-copy -t  text/uri-list "'$clipboard_content'"'
+elif [ $(echo $XDG_SESSION_TYPE | grep -E -i 'x11|xorg') ];then
+# X11
+binary_exec="xclip";
+copy_command="echo '${clipboard_content}' | xclip -sel clip -t text/uri-list"
+else
+# error, use klipper
+echo Error: Unable to identify session type. Switching to klipper...
+dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.setClipboardContents string:"${clipboard_content}"
+return
+fi
+#  Check if the exec is installed.
+if command -v $binary_exec >/dev/null 2>&1; then
+# run the command
+bash -c "${copy_command}"
+if [ "$?" != "0" ]; then
+# failed, use klipper
+echo Error: Failed to restore clipboard content using: $binary_exec
+dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.setClipboardContents string:"${clipboard_content}"
+fi
+return
+else
+# failed, use klipper
+echo Error: Failed to restore clipboard content: $binary_exec is not installed
+dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.setClipboardContents string:"${clipboard_content}"
+return
+
+fi
+}
+
+
 #additional attributes to pass if necessary
 parameter=""
 
 original_clipboard_content="$(dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.getClipboardContents | grep -v "method return time=" | sed -r '1s/^\s+//g' | sed ' s/^string "//' | sed '$ s/"$//')"
-sleep 0.1
-# "clear" the clipboard.
-# by using setClipboardContents the clipboard history is not cancelled and the last copied element can be restored later.
-dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.setClipboardContents string:" "
 [ "$?" != "0" ] && exit 1
+sleep 0.1
+
 #search for the process of dolphin
 windows=$($qdbus_command | grep -i dolphin)
 dolphin=""
@@ -46,16 +92,20 @@ is_active_window=$(dbus-send --session --print-reply --dest=$w /dolphin/Dolphin_
 
 done
 
-
 [ "$dolphin" = "" ] &&  exit 1
+
+# sets the clipboard contents to " " to remove possible paths previously copied by the user.
+dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.setClipboardContents string:" "
+[ "$?" != "0" ] && exit 1
 
 #sends the signal to dolphin to copy the location of the current file to the clipboard.
 dbus-send --session --print-reply --type=method_call --dest=$dolphin /dolphin/Dolphin_1/actions/copy_location org.qtproject.Qt.QAction.trigger
-[ "$?" != "0" ] && exit 1
+[ "$?" != "0" ] && restore_clipboard_content "$original_clipboard_content" && exit 1
+
 sleep 0.1
 #gets clipboard contents from klipper
 path="$(dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.getClipboardContents | grep string | cut -d'"' -f2)"
-[ "$?" != "0" ] && exit 1
+[ "$?" != "0" ] && restore_clipboard_content "$original_clipboard_content" && exit 1
 
 
 
@@ -64,19 +114,19 @@ path="$(dbus-send --session --print-reply --type=method_call --dest=org.kde.klip
 # if no file is selected dolphin does not copy the path and this can be used to understand if there was a file selected or not.
 if [ ! -e "$path" ]; then
 dbus-send --session --print-reply --type=method_call --dest=$dolphin /dolphin/Dolphin_1/actions/edit_select_all org.qtproject.Qt.QAction.trigger
-[ "$?" != "0" ] && exit 1
+[ "$?" != "0" ] && restore_clipboard_content "$original_clipboard_content" && exit 1
 sleep 0.1
 #enables copying the file path even if multiple files are selected (plasma 6)
 dbus-send --session --print-reply --type=method_call --dest=$dolphin /dolphin/Dolphin_1/actions/copy_location org.qtproject.Qt.QAction.resetEnabled
 #gets file location
 dbus-send --session --print-reply --type=method_call --dest=$dolphin /dolphin/Dolphin_1/actions/copy_location org.qtproject.Qt.QAction.trigger
-[ "$?" != "0" ] && exit 1
+[ "$?" != "0" ] && restore_clipboard_content "$original_clipboard_content" && exit 1
 sleep 0.1
 path=$(dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.getClipboardContents | grep string | cut -d'"' -f2)
-[ "$?" != "0" ] && exit 1
+[ "$?" != "0" ] && restore_clipboard_content "$original_clipboard_content" && exit 1
 #deselect what was previously selected (aesthetic reason)
 dbus-send --session --print-reply --type=method_call --dest=$dolphin /dolphin/Dolphin_1/actions/invert_selection org.qtproject.Qt.QAction.trigger
-[ "$?" != "0" ] && exit 1
+[ "$?" != "0" ] && restore_clipboard_content "$original_clipboard_content" && exit 1
 
 
 # the parent folder becomes the path.
@@ -91,12 +141,13 @@ elif [ -e "$path" ]; then
 parameter="-s"
 fi
 
-
-
-
 #restore clipboard content
-#Note: this unfortunately restores everything as text and therefore it is impossible to paste files, etc, even if the full path is in the clipboard.
-dbus-send --session --print-reply --type=method_call --dest=org.kde.klipper /klipper org.kde.klipper.klipper.setClipboardContents string:"$original_clipboard_content"
-
+restore_clipboard_content "$original_clipboard_content"
 #run quick view
 ~/.config/quick_view/quick_view.pyz "$path" $parameter
+
+
+
+
+
+
